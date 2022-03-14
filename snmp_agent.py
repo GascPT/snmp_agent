3ce73753e1806d8248ea017a000b0172e36b07cb
Guilherme Cardoso <37408949+GascPT@users.noreply.github.com>
2022-03-14 14:01:47 -0700

    Merge with SR Linux Folder

--- snmp_agent.py
+++ snmp_agent.py
@@ -4,6 +4,7 @@
 from unittest import result
 import grpc
 import datetime
+import time
 import sys
 import logging
 import socket
@@ -11,6 +12,8 @@ import os
 import ipaddress
 import json
 import signal
+import subprocess
+import threading
 
 import sdk_service_pb2
 import sdk_service_pb2_grpc
@@ -30,7 +33,7 @@ import sdk_common_pb2
 from logging.handlers import RotatingFileHandler
 
 # Modules
-from pygnmi.client import gNMIclient
+from pygnmi.client import gNMIclient,telemetryParser
 
 # Variables
 
@@ -42,7 +45,6 @@ agent_name='snmp_agent'
 ## Open a GRPC channel to connect to sdk_mgr on the dut
 ## sdk_mgr will be listening on 50053
 ############################################################
-#channel = grpc.insecure_channel('unix:///opt/srlinux/var/run/sr_sdk_service_manager:50053')
 channel = grpc.insecure_channel('127.0.0.1:50053')
 metadata = [('agent_name', agent_name)]
 stub = sdk_service_pb2_grpc.SdkMgrServiceStub(channel)
@@ -57,8 +59,48 @@ def get_app_id(app_name):
     logging.info(f'app_id_response {app_id_response.status} {app_id_response.id} ')
     return app_id_response.id
 
+
+
+def subscribe_thread():
+    host = ('unix:///opt/srlinux/var/run/sr_gnmi_server',57400)
+    path = ["/interface[name=mgmt0]/admin-state"]
+    #path = ["/"]
+    cert = "/opt/srlinux/bin/server-cert.pem"
+
+    #update = [('interface[name=ethernet-1/1]', { 'admin-state': 'enable' } )]
+
+    subscribe = {
+            'subscription': [
+                {
+                    'path': 'interface[name=ethernet-1/1]/admin-state',
+                    'mode': 'on_change',
+                    'sample_interval': 1000000000
+                }
+            ],
+            'use_aliases': False,
+            'mode': 'stream',
+            'encoding': 'json_ietf'
+        }
+    #('/bfd/subinterface[name=system0.0]', { 'admin-state': 'enable' } )
+    
+    #with gNMIclient(target=host, username='admin', password='admin', insecure=True, path_cert=cert, debug=True) as gc:
+    #with gNMIclient(target=host, username='admin', password='admin', insecure=True, debug=True) as gc:    
+    with gNMIclient(target= host, username='admin', password='admin', insecure=True) as gc:
+        #result = gc.capabilities()
+        result = gc.get(path=path,encoding='json_ietf')
+        #result = gc.set(update=update,encoding='json_ietf')
+        #result_formatted = json.dumps(result)
+        #print(f"GOT this {result}")
+        #logging.info(result["notification"][0]['update'])
+        telemetry_stream = gc.subscribe(subscribe=subscribe)
+
+        for telemetry_entry in telemetry_stream:
+            #print(telemetryParser(telemetry_entry))
+            logging.info(f"\n\n{telemetryParser(telemetry_entry)}\n")
+            logging.info("HERRRRRRRRRRRRRRRRRRRRRRRRRRRRRREEEEEEEEEEEEEEEEEEEEEE")
+
 ##################################################################################################
-## This is the main proc where all processing for snmp_agent starts.
+## This is the main kproc where all processing for snmp_agent starts.
 ## Agent registeration, notification registration, Subscrition to notifications.
 ## Waits on the sunscribed Notifications and once any config is received, handles that config
 ## If there are critical errors, Unregisters the snmp_agent gracefully.
@@ -67,32 +109,16 @@ def Run():
     response = stub.AgentRegister(request=sdk_service_pb2.AgentRegistrationRequest(), metadata=metadata)
     logging.info(f"Registration response : {response.status}")
 
-    app_id = get_app_id(agent_name)
-    app_id = get_app_id(agent_name)    
-    
     ####################################################################################
-    host = ('unix:///opt/srlinux/var/run/sr_gnmi_server',57400)
-    path = ['/interface[name=mgmt0]/admin-state']
-    cert = "/opt/srlinux/bin/client-cert.pem"
-    
-    with gNMIclient(target=host, username='admin', password='admin', insecure=True, path_cert=cert, debug=True) as gc:
-    #with gNMIclient(target=host, username='admin', password='admin', insecure=True) as gc:
-        result = gc.capabilities()
-        #result = gc.get(path=path,encoding='json_ietf')
-        logging.info(result)
-    
-    logging.info(f"GOT PATH {path}")
-    
+    x = threading.Thread(target=subscribe_thread)
 
+    x.start()
 
-
-    ####################################################################################3
-    if not app_id:
-        logging.error(f'idb does not have the appId for {agent_name} : {app_id}')
-    else:
-        logging.info(f'Got appId {app_id} for {agent_name}')
+    while True:
+        time.sleep(10)
+        logging.info("ENtrou")
   
-    
+ 
     sys.exit()
     return True
 

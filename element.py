1ba31f1aa56c903e81012c9032bed2a360397e35
Guilherme Cardoso <37408949+GascPT@users.noreply.github.com>
2022-04-06 23:23:55 +0100

    process Entry

--- element.py
+++ element.py
@@ -1,7 +1,7 @@
 #"monitoring_elements":[
 #        {
 #            "resource":"interface[name=*]/",
-#            "parameter":"admin-state", 
+#            "parameter":"oper-state", 
 #            "monitoring_condition":{
 #                "admin-state":"enable"
 #            },
@@ -16,14 +16,81 @@ class Element:
                 parameter, 
                 monitoring_condition,
                 resource_filter, 
-                trigger_condition):
+                trigger_condition,
+                watched):
 
         self._resource = resource
         self._parameter = parameter
         self._monitoring_condition = monitoring_condition
         self._resource_filter = resource_filter
         self._trigger_condition = trigger_condition
+        self._watched = watched
+        self._paths = []
+        self._previous_status = ""
 
+    def isWatched(self):
+        return self._watched
+
+    def setWatched(self,watched):
+        self._watched = watched
+        return self._watched
+
+    def getPath(self):
+        return self._resource + self._parameter
+
+    def getParameter(self):
+        return self._parameter
+    
+    def getResource(self):
+        return self._resource
+
+    def getMonitoring(self):
+        return self._monitoring_condition
+    
+    def getKey(self):
+        return self._resource
+
+    def getFilter(self):
+        return self._resource_filter
+
+    def getJSONElement(self):
+        e = {
+            'element': {
+                'parameter' : 
+                    {'value': f'{self._parameter}'},
+                'monitoring_condition' : 
+                    {'value': f'{self._monitoring_condition}'},
+                'resource_filter' : 
+                    {'value': f'{self._resource_filter}'},
+                'trigger_condition' : 
+                    {'value': f'{self._trigger_condition}'}
+            }
+        }
+        #log.info(json.dumps(e,indent = 4))
+        return e
+
+    def addPaths(self,path):
+        self._paths.append(path)
+
+    def getPaths(self):
+        return self._paths
+
+    def getTrigger(self):
+        return self._trigger_condition
+
+    def getStatus(self):
+        return self._previous_status
+
+    def setStatus(self,status):
+        self._previous_status = status
+    
+    def verifyIfExistPath(self, entry_path):
+        if entry_path in self._paths:
+            return True
+        return False 
+
+    def print(self):
+        return f"Element -> \n Resource : {self._resource}\n Parameter : {self._parameter}\n Monitoring : {self._monitoring_condition}\n Resource Filter : {self._resource_filter}\n Trigger Condition : {self._trigger_condition}\n"
 
     
     
--- snmp_agent.py
+++ snmp_agent.py
@@ -15,7 +15,7 @@ import signal
 import subprocess
 import threading
 import queue
-
+import re
 
 import sdk_service_pb2
 import sdk_service_pb2_grpc
@@ -32,7 +32,7 @@ import config_service_pb2
 import telemetry_service_pb2
 import telemetry_service_pb2_grpc
 import sdk_common_pb2
-#from logging.handlers import RotatingFileHandler
+
 
 from logger import *
 from element import *
@@ -41,7 +41,6 @@ from element import *
 from pygnmi.client import gNMIclient,telemetryParser
 
 # Variables
-
 ############################################################
 ## Agent will start with this name
 ############################################################
@@ -61,13 +60,12 @@ host = ('unix:///opt/srlinux/var/run/sr_gnmi_server', 57400)
 
 queue = queue.Queue()
 
-
 log = MyLogger("Logger")
 
 #global_paths = ['interface[name=*]/admin-state']
-global_paths = ['interface[name=ethernet-1/1]/admin-state',
-                'interface[name=ethernet-1/2]/admin-state']
-
+#global_paths = ['interface[name=ethernet-1/1]/oper-state',
+#                'interface[name=ethernet-1/2]/oper-state']
+global_paths = []
 targets = []
 elements = []
 ############################################################
@@ -107,7 +105,7 @@ def Add_Telemetry(js_path, js_data):
     telemetry_info = telemetry_update_request.state.add()
     telemetry_info.key.js_path = js_path
     telemetry_info.data.json_content = js_data
-    log.info(f"Telemetry_Update_Request :: {telemetry_update_request}")
+    #log.info(f"Telemetry_Update_Request :: {telemetry_update_request}")
     telemetry_response = telemetry_stub.TelemetryAddOrUpdate(request=telemetry_update_request, metadata=metadata)
     return telemetry_response
 
@@ -120,11 +118,11 @@ def Delete_Telemetry(js_path):
     telemetry_delete_request = telemetry_service_pb2.TelemetryDeleteRequest()
     telemetry_delete = telemetry_delete_request.key.add()
     telemetry_delete.js_path = js_path
-    log.info(f"Telemetry_Delete_Request :: {telemetry_delete_request}")
+    #log.info(f"Telemetry_Delete_Request :: {telemetry_delete_request}")
     telemetry_response = telemetry_stub.TelemetryDelete(request=telemetry_delete_request, metadata=metadata)
     return telemetry_response
 
-def addTargetsToTelemetry():
+def addTargetsToTelemetry() -> None:
     global targets
     js_path = '.' + agent_name
     json_content = { "target": { "address": [ ] } }
@@ -132,8 +130,22 @@ def addTargetsToTelemetry():
     for t in targets:
         json_content['target']['address'].append({"value": f'{t}'})  
     
-    log.info(json_content)
     r = Add_Telemetry(js_path,json.dumps(json_content))
+
+def addElementsToTelemetry() -> None:
+    global elements
+    base_path = '.' + agent_name + ".monitoring_elements.element"
+    for te in elements:
+        js_key = te.getKey()
+        js_path = base_path + f'{{.resource=="{js_key}"}}'
+        json_content = te.getJSONElement()  
+        r = Add_Telemetry(js_path,json.dumps(json_content))
+
+
+def addBrackets(phrase: str) -> str:
+    if not phrase.endswith('/'):
+        phrase = phrase + '/'
+    return phrase
 
 def addStatusToMemory(obj, filename = None):
     global targets, elements
@@ -149,8 +161,8 @@ def addStatusToMemory(obj, filename = None):
                         t = target['value']
                         targets.append(t)
                 addTargetsToTelemetry() 
+
                 # Add to the File
-                
                 with open("input_elements","r+") as f:
                     file_data = json.load(f)
                     aux_targets = []
@@ -168,36 +180,34 @@ def addStatusToMemory(obj, filename = None):
                     f.write(json.dumps(file_data, indent=4))
                     f.truncate()
                 
-                log.info(targets)
         # Check if are configuration of a element
-        elif obj.config.key.js_path == ".snmp_agent.monitoring_elements.elements":
+        elif obj.config.key.js_path == ".snmp_agent.monitoring_elements.element":
             #Check if exits any config 
             if not obj.config.data.json == "{\n}\n":
-                element_json = json.loads(obj.config.data.json)['elements']
-                log.info(element_json)
-      
-                resource = obj.config.key.keys
-   
+                element_json = json.loads(obj.config.data.json)['element']
+                # Desiralization of the elements into variables
+                resource = obj.config.key.keys[0]
                 parameter = element_json['parameter']['value']
-                monitoring_condition = element_json['monitoring_condition']['value']
-  
+                monitoring_condition = element_json['monitoring_condition']['value'] 
                 if "resource_filter" in element_json:
                     resource_filter = element_json['resource_filter']['value']
                 else:
                     resource_filter = ""
-   
-                
+                   
                 if "trigger_condition" in element_json:
                     trigger_condition = element_json['trigger_condition']['value']
                 else:
                     trigger_condition = ""
   
-                e = Element(resource,parameter,monitoring_condition,resource_filter,trigger_condition)
+                e = Element(resource,parameter,monitoring_condition,resource_filter,trigger_condition, False)
 
+                # Add element to monitoring to the list of elements
                 elements.append(e)
-  
-                log.info(resource)
-                log.info(parameter)
+                # Add to Telemetry
+                addElementsToTelemetry()
+                # Add to the File
+                # TODO
+                
 
 
     # From File
@@ -214,6 +224,33 @@ def addStatusToMemory(obj, filename = None):
                 # Add Targets to State
                 if not len(targets) == 0:
                     addTargetsToTelemetry()
+
+                # Add Elements to the global variable
+                for element in file_data['monitoring_elements']:
+                    resource = addBrackets(element['resource'])
+                    parameter = element['parameter']
+                    monitoring_condition = element['monitoring_condition']
+                    if "resource_filter" in element:
+                        resource_filter = element['resource_filter']
+                    else:
+                        resource_filter = ""
+                    
+                    if "trigger_condition" in element:
+                        trigger_condition = element['trigger_condition']
+                    else:
+                        trigger_condition = ""
+                    
+                    trigger_message = element['trigger_message']
+
+                    e = Element(resource,parameter,monitoring_condition,resource_filter,trigger_condition, False)
+
+                    log.info(e.print())
+
+                    # Add element to monitoring to the list of elements
+                    elements.append(e)
+                    # Add to Telemetry
+                    addElementsToTelemetry()
+
                     
         except Exception as e:
             logging.info(f"Exception caught while reading file :: {e}")
@@ -259,7 +296,6 @@ def send_keep_alive():
 
 ############################################################
 ## Function to subscribe to the paths
-## using telemetry -- add/update info from state 
 ############################################################
 def subscribe_thread(paths):
     subscribe = {
@@ -278,8 +314,7 @@ def subscribe_thread(paths):
                     'sample_interval': 1000000000
                 }
         )
-            
-      
+             
     with gNMIclient(target= host, username='admin', password='admin', insecure=True, debug = True) as gc:
         telemetry_stream = gc.subscribe(subscribe=subscribe)
 
@@ -289,18 +324,130 @@ def subscribe_thread(paths):
             if not "sync_response" in telemetry_entry_str :
                 queue.put(telemetry_entry_str)
 
+def getGNMIPath(path):
+    with gNMIclient(target = host, username='admin', password='admin', insecure=True, debug = True) as gc:
+        return gc.get(path,encoding="json_ietf")
 
+# TODO
 def sendSNMPTrap(entry,target):
-
-
+    
     return None
+
 
 def sendToAllTargets(entry):
     global targets
-
     for target in targets:
         sendSNMPTrap(entry, target)
     
+
+def addGlobalPaths():
+    global global_paths,elements
+    for element in elements:
+        path = element.getPath()
+        global_paths.append(path)
+
+def verifyTriggerCondition(element,value):
+    trigger = element.getTrigger()
+
+    if element.getStatus() == "": # If previous status are empty
+        element.setStatus(value)
+        return False
+    
+    if "->" in trigger: # CHange from value 1 to value 2
+        val1,val2 = trigger.split("->")
+        if element.getStatus() == val1 and val2 == value:
+            element.setStatus(value)
+            return True
+        element.setStatus(value)
+
+    elif "!=" in trigger: # TODO Different from one value
+        log.info("TODO: Different from one value to another")
+
+    return False
+
+
+def verifyMonitoringCondition(element, entry_path):
+    m = element.getMonitoring()
+    if m == "":
+        return True
+    parameter,value = m.split(":")
+
+    if element.getParameter() in entry_path: 
+        entry_path = '/'.join(entry_path.split('/')[:-1])
+
+    path = entry_path +"/"+ parameter
+    # Get Request to assert with the value
+    result = getGNMIPath(path)
+
+    if result['notification'][0]['update'][0]['val'] == value:
+        return True
+
+    return False
+
+
+def processEntry(entry):
+    global elements
+    for element in elements:
+        path = element.getResource()
+        # Can be more than one entry from subscribe
+        for e in entry:
+            entry_path = e['path'].split(":")[1] # Clean the path model
+            
+            if isinstance(e['val'],str):
+                value = e['val']
+                aux_path = entry_path
+                entry_path = '/'.join(entry_path.split('/')[:-1])+"/"
+            else:
+                value = list(e['val'].items())[0][1]
+                aux_path = entry_path+"/"+list(e['val'].keys())[0]
+            #log.info(entry_path + ":" + value)
+            if element.verifyIfExistPath(entry_path):
+                log.info("Enter")
+                if verifyTriggerCondition(element,value):
+                    #Generate SNMP TRAP
+                    log.info(element.getPaths())
+                    log.info("Send TRAP - 1")
+            # This below is first entry process    
+            else:
+                # Path with * 
+                if '*' in path:
+                    resource_filter = element.getFilter()
+                    id1 = entry_path.index("=")
+                    id2 = entry_path.index("]")
+                    sub_path = entry_path[id1 + len("") + 1: id2]
+                    for filt in resource_filter:
+                        # Filter with *
+                        if '*' in filt:
+                            sub_filter = filt[:-1]
+                            if sub_filter in sub_path:
+                                # Process Get Request
+                                if verifyMonitoringCondition(element,entry_path):
+                                    # Add to pathsofElement in that element
+                                    if not entry_path in element.getPaths():
+                                        element.addPaths(entry_path+"/"+element.getParameter())
+                                    # Check trigger condition
+                                    if verifyTriggerCondition(element,value):
+                                        #Generate SNMP TRAP
+                                        log.info("Send TRAP - 2")
+                                
+                        else:
+                        # TODO Specific Filter
+                            if filt == sub_path:
+                                log.info("Second filter")
+                # Specific Path    
+                # Because is a specific path dont check filter options       
+                else:
+                    log.info(element.getResource()+"||||"+entry_path)
+                    if entry_path == element.getResource():
+                        if verifyMonitoringCondition(element,entry_path):
+                            # Add to pathsofElement in that element
+                            if not entry_path in element.getPaths():
+                                element.addPaths(entry_path)
+                                
+                                # Check trigger condition
+                                if verifyTriggerCondition(element,value):
+                                    #Generate SNMP TRAP
+                                    log.info("Send TRAP - 3")
 
 
 
@@ -333,14 +480,17 @@ def Run():
     notificationStreamThread = threading.Thread(target=NotificationStreamThread, args=(stream_id,))
     notificationStreamThread.start()
 
-    #if os.path.exists('input_elements'):
-    #    addStatusToMemory(None,"input_elements")
-   
+    if os.path.exists('input_elements'):
+        addStatusToMemory(None,"input_elements")
+
     ####################################################################################
-    # START OF SUBSCRIBE GNMI THREAD
+    # Add Global Paths from Elements
+    addGlobalPaths()
+
+    ####################################################################################
+    # START OF GNMI SUBSCRIBE THREAD
 
     x = threading.Thread(target=subscribe_thread, args=(global_paths,))
-    #print(global_paths)
     x.start()
     
     ################################################################################
@@ -350,12 +500,10 @@ def Run():
         while not queue.empty():
             entry = queue.get()
             entry = entry['update']['update']
+            #log.info(f"{entry}  :::::  QUEUE SIZE --> {queue.qsize()}\n ")
+            processEntry(entry)
 
-            sendToAllTargets(entry)
 
-            log.info(f"{entry}  :::::  QUEUE SIZE --> {queue.qsize()}\n ")
- 
-        
     sys.exit()
     return True
 
@@ -390,4 +538,3 @@ if __name__ == '__main__':
         log.info('Agent unregistered and agent routes withdrawed from dut')
     else:
         log.info(f'Some exception caught, Check !')
-
